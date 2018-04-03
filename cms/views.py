2a825ea7fa7393c49c3b2f45a8e818281bc748dc
apayol <apayolmontero@gmail.com>
2018-04-03 23:53:23 +0200

    Views modificado

--- cms/views.py
+++ cms/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
+from cms.models import Pages
+from django.http import HttpResponse
 
 # Create your views here.
+
+def inicio(resp):
+    paginas = Pages.objects.all()
+    respuesta = ""
+    for pagina in paginas:
+        respuesta = pagina.name + ": " + pagina.page + "<br>" + respuesta
+    return HttpResponse(respuesta)
+
+def pagina(resp, recurso):
+    try:
+        pagina = Pages.objects.get(name=recurso)
+        return HttpResponse(pagina.page)
+    except Pages.DoesNotExist:
+        return HttpResponseNotFound('<h3>La página no existe</h3>')
